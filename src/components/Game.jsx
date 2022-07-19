76b21f3e69765d713e097935e7842bab25d70e15
kbeloin <kyle.beloin@me.com>
2022-07-19 08:20:46 -0700

    Changes to style; history shows reversed.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -35,42 +35,45 @@ export default function Game() {
     <>
       {gameState.gameStarted && (
         <div className={styles.history}>
-          <h2>Game History</h2>
+          <h2>Card</h2>
 
           {/* Skip the first */}
           {gameHistory &&
             gameHistory.length > 1 &&
-            gameHistory.slice(1).map((game, index) => (
-              <div key={`history-${index}`}>
-                <div className={styles["history-item"]}>
-                  <div>
-                    {/* long dash*/}
-                    {game.attempt}
+            gameHistory
+              .slice(1)
+              .reverse()
+              .map((game, index) => (
+                <div key={`history-${index}`}>
+                  <div className={styles["history-item"]}>
+                    <div>
+                      {/* long dash*/}
+                      {game.attempt}
+                    </div>
+                    <div>
+                      {/* long dash*/}
+                      &nbsp;&mdash;&nbsp;
+                    </div>
+                    <div>
+                      {`${Math.floor(game.time / 60)}:${
+                        game.time % 60 < 10 ? "0" : ""
+                      }${game.time % 60}`}
+                    </div>
+                    <div>
+                      {/* long dash*/}
+                      &nbsp;&mdash;&nbsp;
+                    </div>
+                    <div>
+                      {game.gameState.gameOver
+                        ? "L"
+                        : game.gameState.gameWon
+                        ? "W"
+                        : "NC"}
+                    </div>
                   </div>
-                  <div>
-                    {/* long dash*/}
-                    &nbsp;&mdash;&nbsp;
-                  </div>
-                  <div>
-                    {`${Math.floor(game.time / 60)}:${
-                      game.time % 60 < 10 ? "0" : ""
-                    }${game.time % 60}`}
-                  </div>
-                  <div>
-                    {/* long dash*/}
-                    &nbsp;&mdash;&nbsp;
-                  </div>
-                  <div>
-                    {game.gameState.gameOver
-                      ? "L"
-                      : game.gameState.gameWon
-                      ? "W"
-                      : "NC"}
-                  </div>
+                  <hr />
                 </div>
-                <hr />
-              </div>
-            ))}
+              ))}
         </div>
       )}
 
